4c390309f3e7b65a7c56687f39a48aeaafa75623
Anh Le <letuananh.ttmmt.ctu@gmail.com>
2020-11-12 10:34:20 +0700

    Add some example from tutorial

--- cmd/example/main.go
+++ cmd/example/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/line/line-bot-sdk-go/linebot"
+)
 
 func main() {
 	fmt.Printf("Hello Line Friends\n")
+
+	req := http.Request{}
+
+	// Initial Client
+	client := &http.Client{}
+	bot, err := linebot.New("<channel secret>", "<channel accsss token>", linebot.WithHTTPClient(client))
+
+	// Parse request
+	events, err := bot.ParseRequest(&req)
+	if err != nil {
+		// Do something when something bad happened.
+	}
+
+	// Loop for events
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			// Do Something...
+		}
+	}
+
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/captain-corgi/golang-line-bot
 
 go 1.15
+
+require github.com/line/line-bot-sdk-go v7.6.0+incompatible
